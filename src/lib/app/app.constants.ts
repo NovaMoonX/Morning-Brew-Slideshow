246c763421e5e59b4ff141aaa7c0f208ada34ebc
xFallingDuskx <nova@moondreams.dev>
2026-05-21 21:45:24 -0400

    finish set up

--- src/lib/app/app.constants.ts
+++ src/lib/app/app.constants.ts
@@ -1,2 +1,2 @@
 export const APP_TITLE = 'Morning Brew Slideshow';
-export const APP_DESCRIPTION = 'Interactable slideshow for Morning Brew daily newsletter issues';
+export const APP_DESCRIPTION = '';
